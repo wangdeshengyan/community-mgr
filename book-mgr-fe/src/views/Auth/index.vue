97daf6778041b3c362dc08b8ce6aab4113550306
xiaozhang <2975634718@qq.com>
2021-03-18 21:22:52 +0800

    完成前端登入注册相关的界面

---
+++ book-mgr-fe/src/views/Auth/index.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="auth">
+        <div class="bg"></div>
+       <div class="title-info">
+<img src="https://img.88tph.com/tphc.1/production/20180523/12528590.jpg" alt="">
+&nbsp;&nbsp;<h2 class="title">社区团购系统 前台</h2>
+       </div>
+       <div class="form">
+<a-tabs >
+    <a-tab-pane key="1"  tab="登入">
+        <div class="item">
+         <a-input size="large" placeholder="账户  " >
+             <template v-slot:prefix> <UserOutlined /></template> 
+        </a-input>
+        </div>
+         
+    <div class="item">
+         <a-input size="large" placeholder="密码">
+             <template v-slot:prefix>
+                 <LockOutlined />
+             </template>
+             </a-input>     
+
+    </div>
+  <div class="item">
+         <a href="">忘记密码 </a>  
+  </div>
+  <div class="item">
+      <a-button type="primary">
+          登入
+      </a-button>
+      </div> 
+    </a-tab-pane>
+    <a-tab-pane key="2" tab="注册" >
+     <div class="item">
+         <a-input size="large" placeholder="账户"> <template v-slot:prefix> <UserOutlined /></template> 
+        </a-input>  
+         
+        </div>
+    <div class="item">
+         <a-input size="large" placeholder="密码">
+              <template v-slot:prefix> <LockOutlined /></template> 
+        
+         </a-input>
+
+
+    </div>
+    <div class="item">
+         <a-input size="large" placeholder="邀请码">
+             <template v-slot:prefix> <MailOutlined /></template>
+         </a-input>
+         
+    </div>
+  <div class="item">
+      <a-button type="primary">
+          注册
+      </a-button>
+  </div>
+  
+    </a-tab-pane>
+</a-tabs>
+
+       </div>
+    </div>
+</template>
+
+<script src='./index.js'></script>
+
+<style lang="scss" scoped>
+@import './index.scss';
+</style>
